98f705e8e78e7ed5943563df82a898e44621e328
BL <bhushankumar.lilapara@gmail.com>
2020-12-12 15:16:04 +0530

    docker clean up script

--- ubuntu/docker-clean.sh
+++ ubuntu/docker-clean.sh
@@ -1,4 +1,4 @@
 docker image prune --force
 docker image prune -a --force
-docker container prune --filter "until=240h" --force
 docker builder prune --force
+# docker container prune --filter "until=240h" --force
