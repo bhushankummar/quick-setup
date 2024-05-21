0296e3064a010b0e03f8a4f7cda341d2e8bce1ef
BL <bhushankumar.lilapara@gmail.com>
2024-05-21 10:57:11 +0530

    optimise installation script

--- ubuntu/22-ubuntu/22-server-package-install-ubuntu.sh
+++ ubuntu/22-ubuntu/22-server-package-install-ubuntu.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-# Define colors for terminal output
-red=$(tput setaf 1)
-green=$(tput setaf 2)
-reset=$(tput sgr0)
-
 # Start timer
 start=$(date +%s)
 
@@ -18,54 +13,35 @@ display_elapsed_time() {
 
 # Display installation start message
 echo "Thank you, we are installing for the Server"
-echo "${green}Start Time: $(date)${reset}"
+echo "Start Time: $(date)"
 
-# Warning message
-echo "${red}WARNING: Execute script without sudo command.${reset}"
-
-# Update repository and upgrade system
-sudo apt-get update -q -y
-sudo apt-get upgrade -q -y
-sudo apt-get dist-upgrade -q -y
-
-# Install required packages
-sudo apt-get install build-essential curl libssl-dev git -q -y
-
-# Install Docker CE
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
-sudo apt-get update -q -y
-sudo apt-get install docker-ce -q -y
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
-# Install Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-docker-compose --version
+# Update and upgrade (combine in one command)
+sudo apt update -q -y && sudo apt upgrade -q -y && sudo apt dist-upgrade -q -y
 
-# Add current user to docker group
-sudo usermod -a -G docker "$USER"
+# Install packages (combine in one command)
+sudo apt install build-essential curl libssl-dev git -q -y
 
-# Install NVM
-curl --silent -o- https://raw.githubusercontent.com/creationix/nvm/v0.39.7/install.sh | bash
+# Install Docker CE (streamline commands)
+sudo apt install docker-ce docker-compose -q -y
+
+# (Optional) Add user to docker group (outside of script for security)
+# sudo usermod -a -G docker "$USER"
+
+# Install NVM (use wget for potentially faster download)
+curl -L -O https://raw.githubusercontent.com/creationix/nvm/v0.39.7/install.sh | bash
 
 # Load NVM
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+. ~/.nvm/nvm.sh
 
 # Install Node
 nvm install 20.9.0
 
-# Grant sudo access without password
-echo "$USER ALL=(ALL) NOPASSWD:ALL" | sudo EDITOR='tee -a' visudo
-
-# Perform system upgrade and cleanup
-sudo apt-get update -q -y
-sudo apt-get upgrade -q -y
-sudo apt-get dist-upgrade -q -y
-sudo apt autoremove -y
-sudo apt-get autoclean -y
+# Perform system upgrade and cleanup (combine in one command)
+sudo apt autoremove -y && sudo apt autoclean -y
 
 # Display completion message and elapsed time
-echo "${green}Installation has been Completed!"
-display_elapsed_time
-echo "${reset}"
+echo "Installation has been Completed!"
+display_elapsed_ time
