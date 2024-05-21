d3ec513b67c883a5f62ea4d29b99c23c2c6446c8
BL <bhushankumar.lilapara@gmail.com>
2024-05-21 11:00:18 +0530

    allow auto install docker

--- ubuntu/22-ubuntu/22-server-package-install-ubuntu.sh
+++ ubuntu/22-ubuntu/22-server-package-install-ubuntu.sh
@@ -16,7 +16,7 @@ echo "Thank you, we are installing for the Server"
 echo "Start Time: $(date)"
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
 
 # Update and upgrade (combine in one command)
 sudo apt update -q -y && sudo apt upgrade -q -y && sudo apt dist-upgrade -q -y
