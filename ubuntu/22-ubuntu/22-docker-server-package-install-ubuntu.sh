59dac1b00ccd49da3a652c06c117decac8fbf72e
BL <bhushankumar.lilapara@gmail.com>
2024-10-19 12:20:42 +0530

    update changes for the installation

--- ubuntu/22-ubuntu/22-docker-server-package-install-ubuntu.sh
+++ ubuntu/22-ubuntu/22-docker-server-package-install-ubuntu.sh
@@ -32,7 +32,7 @@ sudo apt-get update -q -y
 sudo apt-get install docker-ce -q -y
 
 # Install Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 
